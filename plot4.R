cc189197ac796adee953432a10f6e92e915d1561
vikasrl <104867760+vikasrl@users.noreply.github.com>
2022-05-04 02:51:26 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,15 @@
+library(dplyr)
+library(ggplot2)
+g <- readRDS("summarySCC_PM25.rds")
+f <- readRDS("Source_Classification_Code.rds")
+SCCsofCoal <- f 
+  filter(grepl("coal", EI.Sector, ignore.case=TRUE))
+  select(f)
+emissionsyear <- g
+  filter(f %in% SCCsofCoal$f)
+  group_by(year) 
+  summarize(allemissions = sum(Emissions))
+emissionsyear$year <- as.factor(emissionsyear$year)
+ggplot(emissionsyear, aes(x=year, y=allemissions)) + geom_bar(stat="identity") + labs(x="Year", y="PM2.5 Emissions (tons)") + ggtitle("US PM2.5 Emissions from Coal Related Sources")
+dev.copy(png,'plot4.png')
+dev.off()
